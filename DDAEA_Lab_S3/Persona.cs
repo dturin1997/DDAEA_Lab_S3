4c9aaa0d34f57c1cf07d09b893c3af77831e1736
Alumno <Alumno@LAB707-03>
2023-04-03 21:45:52 -0500

    btnPersona click

--- DDAEA_Lab_S3/Persona.cs
+++ DDAEA_Lab_S3/Persona.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,21 @@ namespace DDAEA_Lab_S3
 {
     public partial class Persona : Form
     {
+        SqlConnection conn;
         public Persona()
         {
+            this.conn = conn;
             InitializeComponent();
+        }
+
+        private void Persona_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void btnPersona_Click(object sender, EventArgs e)
+        {
+            Persona persona = new Persona(conn);
+            persona.Show();
         }
     }
 }
