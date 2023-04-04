f3d924f59184db82f21fd024da153a41e90335ea
Alumno <Alumno@LAB707-03>
2023-04-03 21:06:21 -0500

    Boton conectar

--- DDAEA_Lab_S3/Form1.Designer.cs
+++ DDAEA_Lab_S3/Form1.Designer.cs
@@ -124,6 +124,7 @@
             this.button1.TabIndex = 9;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -162,6 +163,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- DDAEA_Lab_S3/Form1.cs
+++ DDAEA_Lab_S3/Form1.cs
@@ -7,15 +7,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace DDAEA_Lab_S3
 {
     public partial class Form1 : Form
     {
-        //Hola
+        //SqlConnection nos permite manejar el acceso al servidor
+        SqlConnection conn;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            //Declaramos variables para almacenar los valores de los TextBox
+            //y definimos una cadena de conexion
+            String servidor = txtServidor.Text;
+            String bd = txtBaseDatos.Text;
+            String user = txtUsuario.Text;
+            String pwd = txtPassword.Text;
+
+            String str = "Server="+servidor+";DataBase="+bd+";";
+
+            //La cadena de conexion cambia en funcion del estado del CheckBox
+            if (chkAutenticacion.Checked)
+                str += "Integrated Security=true";
+            else
+                str += "User Id=" + user + ";Password=" + pwd + ";";
+
+            //Abrir una conexion con el servidor, usando la cadena de conexion
+            try
+            {
+                conn = new SqlConnection(str);
+                conn.Open();
+                MessageBox.Show("Conectado satisfactoriamente");
+                btnDesconectar.Enabled = true;
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show("Error al conectar el servidor: \n" + ex.ToString());
+            }
+
+
+
         }
     }
 }
