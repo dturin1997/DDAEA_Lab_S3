47d81ee768feec8ed04330f86280422b45d8f297
Nicole <Alumno@LAB707-04>
2023-04-03 21:32:47 -0500

    Se creo la interfaz borrador persona

---
+++ DDAEA_Lab_S3/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DDAEA_Lab_S3
+{
+    public partial class Persona : Form
+    {
+        public Persona()
+        {
+            InitializeComponent();
+        }
+    }
+}
